f6a7804aa9a265f8b9cfe5494b71d058959067be
Two-Weeks <webmon@kakao.com>
2021-11-03 23:53:49 +0900

    🐛 feat: toolbar

--- app/src/main/java/com/example/bandy/MainActivity.java
+++ app/src/main/java/com/example/bandy/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.bandy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView ivMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ivMenu = (ImageView) findViewById(R.id.iv_menu);
+
+        ivMenu.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Make notice", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
